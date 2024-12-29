fe668e2edac139fa18925aa12c4de330ead55247
oleHxOleksuk <108345191+oleHxOleksuk@users.noreply.github.com>
2024-12-29 22:51:50 +0200

    edit_phone

--- record.py
+++ record.py
@@ -5,8 +5,7 @@ class Record:
         self.name = Name(name)
         self.phones = []
 
-    def __str__(self):
-        return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}"
+
     
     def add_phone(self, number:str):
          self.phones.append(Phone(number))
@@ -15,11 +14,18 @@ class Record:
          self.phones = list(filter(lambda phone: phone == number, self.phones))
 
     def edit_phone(self, old_number, new_number):
-         self.phones = list(map(
+         for number in self.phones:
+              if number != old_number:
+                raise ValueError(f'Phone {old_number} not found!')
+              else:
+                self.phones = list(map(
               lambda phone: Phone(new_number) if phone.value == old_number else phone, self.phones
               ))
          
     def find_phone(self, number:str):
-         for phone in self.phones:
+        for phone in self.phones:
               if phone.value == number:
                    return phone
+              
+    def __str__(self):
+        return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}"
