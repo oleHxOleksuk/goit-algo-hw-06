144aa445a400f274877fd73b79746b9c9bb34552
oleHxOleksuk <108345191+oleHxOleksuk@users.noreply.github.com>
2024-12-29 17:19:20 +0200

    12435

--- main.py
+++ main.py
@@ -1,38 +1,34 @@
-from collections import UserDict
+from record import Record
+from address_book import AddressBook
 
-class Field:
-    def __init__(self, value):
-        self.value = value
 
-    def __str__(self):
-        return str(self.value)
+book = AddressBook()
 
-class Name(Field):
-    def __init__(self, name):
-        self.value = name
-	
+# Створення запису для John
+john_record = Record("John")
+john_record.add_phone("1234567890")
+john_record.add_phone("5555555555")
 
-class Phone(Field):
-    def __init__(self, number):
-         self.value = check_number(number)
+# Додавання запису John до адресної книги
+book.add_record(john_record)
 
-    def check_number(self, number):
-         if number == 10:
-              return number
-         else:
-              raise ValueError("The phone number must contain 10 digits")
-         
+# Створення та додавання нового запису для Jane
+jane_record = Record("Jane")
+jane_record.add_phone("9876543210")
+book.add_record(jane_record)
 
-class Record:
-    def __init__(self, name):
-        self.name = Name(name)
-        self.phones = []
+for name, record in book.data.items():
+    print(record)
 
-    # реалізація класу
+# Знаходження та редагування телефону для John
+john = book.find("John")
+john.edit_phone("1234567890", "1112223333")
 
-    def __str__(self):
-        return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}"
+print(john)  # Виведення: Contact name: John, phones: 1112223333; 5555555555
 
-class AddressBook(UserDict):
-    # реалізація класу
-		pass
+# Пошук конкретного телефону у записі John
+found_phone = john.find_phone("5555555555")
+print(f"{john.name}: {found_phone}")  # Виведення: John: 5555555555
+
+# Видалення запису Jane
+#book.delete("Jane")
